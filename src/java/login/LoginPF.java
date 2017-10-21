e3d96d545ec32f44664d9c081f2b28657c9ddca9
andres <argd14@gmail.com>
2017-10-21 17:13:58 -0600

    s

---
+++ src/java/login/LoginPF.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package login;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author ramiro
+ */
+@ManagedBean
+@RequestScoped
+public class LoginPF {
+
+    /**
+     * Creates a new instance of LoginPF
+     */
+    public LoginPF() {
+    }
+    
+}
